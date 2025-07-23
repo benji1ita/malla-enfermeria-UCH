bfa6bd40747b32c26fe55e41d82b890a908c2411
benji1ita <benjamin.cerda.g@ug.uchile.cl>
2025-07-22 22:46:52 -0400

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 esto es lo importante
+  base: './'
 })
